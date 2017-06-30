4f34ffc7614858948ac07a33578a4fb4e0232eaa
BuildTools <unconfigured@null.spigotmc.org>
2017-06-30 14:59:53 -0500

    Setup skeleton code for expanse main.

---
+++ code/linux/main.c
@@ -0,0 +1,11 @@
+//----------------------------------------------//
+//      This is the Expanse Game Project        //
+//                      2017                    //
+//              Maintainer/s: Dale Kipple       //
+//----------------------------------------------//
+
+int main(int argc char *argv)
+{
+
+        return 0;                               // Full stop exit with no errors
+}
