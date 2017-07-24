573702e91f4496b71c0fe2457e9e60d128869e63
BuildTools <unconfigured@null.spigotmc.org>
2017-07-23 21:05:15 -0500

    Escape key fixed

--- code/linux/curses_main.c
+++ code/linux/curses_main.c
@@ -27,23 +27,19 @@ int curses_main()
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &crs_winSz);
 
 	initscr();
-	cbreak();
-	keypad(stdscr, TRUE);
+	timeout(0);
+	//keypad(stdscr, TRUE);
 	mousemask(ALL_MOUSE_EVENTS, NULL);
+	noecho();
 
-	printw("Running in curses terminal\n");
-	printw("Press any key to continue\n");
-	refresh();
-	getch();
 	clear();
 
-	while(getch() != 27)
+	while(kb_press != 27)
 	{
 		//Remember mvprintw and other mv functions in curses are always written (columns, rows) or (y,x)
-		mvprintw((crs_winSz.ws_row/2),((crs_winSz.ws_col/2)-(strlen("Press any key with an escape code to leave!")/2)), "Press any key with an escape code to leave!");
-		mvprintw(1,1,"%c",kb_press);
+		mvprintw((crs_winSz.ws_row/2),((crs_winSz.ws_col/2)-(strlen("Press escape to leave!")/2)), "Press escape to leave!");
+		//mvprintw(1,1,"%c",kb_press);
 		refresh();
-		noecho();
 		kb_press = kb_cleanup(getch());
 
 	}
@@ -53,8 +49,12 @@ int curses_main()
 
 char kb_cleanup(char key)
 {
+	int kbhit = 0;
 	if(key == 27)
-		if(getch() == 0) return 27;
+		{
+			if(getch() == -1) return 27;
+			return kbhit;
+		}
 		
 	return 0;
 }
