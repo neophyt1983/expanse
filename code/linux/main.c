ed37c7d7c694b4ebf1bd5a64ecb379825e05cbef
neophyt1983 <neophyt1983@gmail.com>
2017-10-13 17:34:26 -0500

    Corrected file names

--- code/linux/main.c
+++ code/linux/main.c
@@ -14,7 +14,7 @@
 #include <curses.h>
 #include <stdbool.h>
 #include <X11/Xlib.h>
-#include "expanse.h"
+//#include "sdl_main.c"
 
 
 // Definitions
@@ -61,44 +61,3 @@ int main(int argc, char **argv)
 	return 1;
 }
 
-int sdl_main()
-{
-	// Used to check to see if SDL is running in the main loop
-	bool running = true;
-	SDL_Event event;
-	SDL_Window* WinMain = NULL;
-
-	
-	
-	WinMain = SDL_CreateWindow("Expanse",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		640,
-		480,
-		/*SDL_WINDOW_VULKAN*/SDL_WINDOW_OPENGL);
-	
-
-	if ( WinMain == NULL){
-		printf("Could not create window: %s\n", SDL_GetError());
-		return 1;
-	}
-	
-	// Main Loop
-	while(running)
-	{
-		// SDL Event Loop
-		while(SDL_PollEvent(&event))
-		{
-			if(event.type == SDL_QUIT)
-			{
-				running = false;
-			}
-		}
-	}
-	
-	// Close and destroy the window
-	SDL_DestroyWindow(WinMain);	
-	// Clean up
-	SDL_Quit();					
-	return 0;
-}
