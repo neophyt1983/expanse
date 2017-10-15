2b7d9e1b871edbf8565f8cf892eda85a636b2716
neophyt1983 <neophyt1983@gmail.com>
2017-10-15 00:50:01 -0500

    Began building frame for Windows version using Code Blocks IDE
    Added necessary SDL2 files and created main.c that can be compiled in CB 16.01

---
+++ code/windows/expanse/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_timer.h>
+//#include <SDL_image.h>
+#include <SDL_audio.h>
+#include <stdbool.h>
+
+// Definitions
+#undef main
+
+// Functions
+
+int main(int argc, char *argv[], char **envp)
+{
+
+    return 0;
+}
